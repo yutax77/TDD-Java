f29673326c7f92aa9b7d36b7092dc08d94efbd9d
yutax77 <yutax77@gmail.com>
2011-07-05 00:20:50 +0900

    change junit3 to testng

--- src/test/java/com/yutax77/AppTest.java
+++ src/test/java/com/yutax77/AppTest.java
@@ -1,36 +1,16 @@
 package com.yutax77;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.testng.annotations;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
+   /**
      * Rigourous Test :-)
      */
+	@Test
     public void testApp()
     {
         assertTrue( true );
