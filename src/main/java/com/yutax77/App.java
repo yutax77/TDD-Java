0c8183c4140715911f437e76ccd17b6da51bf3ce
yutax77 <yutax77@gmail.com>
2011-07-04 23:46:52 +0900

    Initial commit

---
+++ src/main/java/com/yutax77/App.java
@@ -0,0 +1,13 @@
+package com.yutax77;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
