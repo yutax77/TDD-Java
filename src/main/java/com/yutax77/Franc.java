ccea4f681d2b70596bce790530957d9589cce44b
yutax77 <yutax77@gmail.com>
2011-07-10 13:51:11 +0900

    P31

--- src/main/java/com/yutax77/Franc.java
+++ src/main/java/com/yutax77/Franc.java
@@ -1,19 +1,11 @@
 package com.yutax77;
 
-public class Franc {
-	private int amount; 
-	
+public class Franc extends Money {
 	Franc(int amount){
 		this.amount = amount;
 	}
 
 	Franc times(int multiplier) {
 		return new Franc(amount * multiplier);
-	}
-	
-	@Override
-	public boolean equals(Object object) {
-		Franc franc = (Franc) object;
-		return amount == franc.amount;
 	}
 }
--- src/test/java/com/yutax77/DollarTest.java
+++ src/test/java/com/yutax77/DollarTest.java
@@ -22,5 +22,7 @@ public class DollarTest {
   public void testEquality() {
 	  assertTrue(new Dollar(5).equals(new Dollar(5)));
 	  assertFalse(new Dollar(5).equals(new Dollar(6)));
+	  assertTrue(new Franc(5).equals(new Franc(5)));
+	  assertFalse(new Franc(5).equals(new Franc(6)));
   }
 }
