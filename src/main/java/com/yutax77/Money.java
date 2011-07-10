f050df1e5db296a64437f8469273bed24261cd24
yutax77 <yutax77@gmail.com>
2011-07-10 15:21:00 +0900

    P34

--- src/main/java/com/yutax77/Money.java
+++ src/main/java/com/yutax77/Money.java
@@ -6,6 +6,6 @@ public class Money {
 	@Override
 	public boolean equals(Object object) {
 		Money money = (Money) object;
-		return amount == money.amount;
+		return amount == money.amount && getClass().equals(money.getClass());
 	}
 }
--- src/test/java/com/yutax77/DollarTest.java
+++ src/test/java/com/yutax77/DollarTest.java
@@ -24,5 +24,6 @@ public class DollarTest {
 	  assertFalse(new Dollar(5).equals(new Dollar(6)));
 	  assertTrue(new Franc(5).equals(new Franc(5)));
 	  assertFalse(new Franc(5).equals(new Franc(6)));
+	  assertFalse(new Franc(5).equals(new Dollar(5)));
   }
 }
