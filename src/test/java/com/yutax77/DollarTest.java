323349e3373f346ad1cb395a3bc2e3d0207135ae
yutax77 <yutax77@gmail.com>
2011-07-12 23:15:21 +0900

    P52

--- src/test/java/com/yutax77/DollarTest.java
+++ src/test/java/com/yutax77/DollarTest.java
@@ -22,8 +22,6 @@ public class DollarTest {
   public void testEquality() {
 	  assertTrue(Money.dollar(5).equals(Money.dollar(5)));
 	  assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-	  assertTrue(Money.franc(5).equals(Money.franc(5)));
-	  assertFalse(Money.franc(5).equals(Money.franc(6)));
 	  assertFalse(Money.franc(5).equals(Money.dollar(5)));
   }
   
