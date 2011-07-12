e449aae8b7c13d668ac3978537811753e13b96fd
yutax77 <yutax77@gmail.com>
2011-07-12 22:41:44 +0900

    P43

--- src/main/java/com/yutax77/Dollar.java
+++ src/main/java/com/yutax77/Dollar.java
@@ -1,12 +1,12 @@
 package com.yutax77;
 
 public class Dollar extends Money {
-	Dollar(int amount){
+	Dollar(int amount, String currency){
 		this.amount = amount;
-		currency = "USD";
+		this.currency = currency;
 	}
 
 	Money times(int multiplier) {
-		return new Dollar(amount * multiplier);
+		return Money.dollar(amount * multiplier);
 	}
 }
