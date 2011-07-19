b2b3a91c7be48ef6322faa53d8632430f5afa66b
yutax77 <yutax77@gmail.com>
2011-07-20 00:16:34 +0900

    P63。テストは通った。キャストが醜い。

--- src/main/java/com/yutax77/Bank.java
+++ src/main/java/com/yutax77/Bank.java
@@ -3,7 +3,9 @@ package com.yutax77;
 class Bank {
 
 	Money reduce(Expression source, String to) {
-		return Money.dollar(10);
+		Sum sum = (Sum) source;
+		int amount = sum.augend.amount + sum.addend.amount;
+		return new Money(amount, to);
 	}
 
 }
