d47db189e23733ac19836c3910d1cd7788a861a8
yutax77 <yutax77@gmail.com>
2011-07-20 00:19:31 +0900

    P64

--- src/main/java/com/yutax77/Bank.java
+++ src/main/java/com/yutax77/Bank.java
@@ -4,8 +4,7 @@ class Bank {
 
 	Money reduce(Expression source, String to) {
 		Sum sum = (Sum) source;
-		int amount = sum.augend.amount + sum.addend.amount;
-		return new Money(amount, to);
+		return sum.reduce(to);
 	}
 
 }
